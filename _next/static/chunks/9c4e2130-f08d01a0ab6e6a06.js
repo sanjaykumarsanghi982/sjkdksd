21756a8842f530184aabb662a59be7a24f9383fe
sanjaykumarsanghi982 <sanjaykumarsanghi982@gmail.com>
2025-11-01 13:29:07 +0530

    Add files via upload

---
+++ _next/static/chunks/9c4e2130-f08d01a0ab6e6a06.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[706],{5097:function(t,n,e){e.d(n,{O8Q:function(){return i}});var r=e(1810);function i(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"48",d:"M244 400 100 256l144-144M120 256h292"},child:[]}]})(t)}}}]);
